472417ae43deda0d0851730add4fc9f8684fd06a
Serghei MIHAI <smihai@entrouvert.com>
2015-02-04 10:12:20 +0100

    service_uri raises SSO

--- ckanext/ozwillo_organization_api/plugin.py
+++ ckanext/ozwillo_organization_api/plugin.py
@@ -105,7 +105,7 @@ def create_organization(context, data_dict):
         services = {'services': [{
             'local_id': 'organization',
             'name': , org_dict['title'],
-            'service_uri': organization_uri,
+            'service_uri': organization_uri + '/sso',
             'description': 'Organization ' + org_dict['name'] + ' on CKAN',
             'tos_uri': organization_uri,
             'policy_uri': organization_uri,
